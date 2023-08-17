73ec5fe957210c6a3738ddf0361b680d1b335d8f
justina177 <justinaenenche@yahoo.com>
2023-08-17 15:47:57 +0100

    Implentented api call

--- src/components/AllCountries/AllCountries.jsx
+++ src/components/AllCountries/AllCountries.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom';
+import apiURL from '../utils/api';
 
 const AllCountries = () => {
+  const [countries, setCountries] = useState([]);
+  const [ isLoading, setIsLoading ] =useState(true);
+  const [error, setError] = useState('')
+
+  const getAllCountries = async()=> {
+    try {
+      const res = await fetch(`${apiURL}/all`)
+
+      if(!res.ok) throw new Error("Something went wrong!");
+
+      const data = await res.json();
+
+      console.log(data);
+
+      setCountries(data);
+
+      setIsLoading(false);
+    } catch (error) {
+      setIsLoading(false);
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    getAllCountries();
+  }, []);
+
   return (
-    <div>AllCountries</div>
+    <div className="all__country__wrapper">
+      <div className="courty__top"></div>
+
+      <div className="country__bottom">
+      {isLoading && !error && <h4>Loading........</h4>}
+        {error && !isLoading && <h4>{error}</h4>}
+
+        {countries?.map((country) => (
+          <Link to={`/country/${country.name.common}/all`}>
+            <div className="country__card">
+              <div className="country__img">
+                {/* <img src={country.flags.png} alt="" /> */}
+              </div>
+
+              <div className="country__data">
+                <h3>{country.name.common}</h3>
+                <h6>
+                  {" "}
+                  Population:{" "}
+                  {new Intl.NumberFormat().format(country.population)}
+                </h6>
+                <h6> Region: {country.region}</h6>
+                <h6>Capital: {country.capital}</h6>
+              </div>
+            </div>
+          </Link>
+        ))}
+        
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/utils/api.js
@@ -0,0 +1,3 @@
+const apiURL = 'https://restcountries.com/v3.1/all?fields=name,flags'
+
+export default apiURL
